eaba498abfb4f0d048e69e59077b2310ca824302
Your Name <pc@pc.com>
2025-07-08 17:49:00 +0900

    5:48

---
+++ vue-project/src/views/EventClick.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3>Event</h3>
+
+  <p :key="i" v-for="(num, i) in list">
+    <button @click="ClickFnc(num)">이벤트클릭</button>
+  </p>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "",
+      list: [10, 20, 30],
+    };
+  },
+  methods: {
+    ClickFnc(num) {
+      alert(num + " 클릭함");
+    },
+  },
+};
+</script>
