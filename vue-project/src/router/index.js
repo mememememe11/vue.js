063d3723b802ef7c154fbbb7e8ed9424967dbff3
Your Name <pc@pc.com>
2025-07-09 09:55:08 +0900

    9:55

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import DataBinding from "../views/EventClick.vue";
+import DataBinding from "../views/ComputedView.vue";
 
 const routes = [
   {
