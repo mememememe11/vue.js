9bb1e87bb2dd14e0446fb7ba112b9ad0e01ebfb5
Your Name <pc@pc.com>
2025-07-10 13:05:44 +0900

    13:05

--- vue-project/src/store/index.js
+++ vue-project/src/store/index.js
@@ -6,7 +6,7 @@ export default createStore({
   state: {
     todos: [
       { id: 1, name: "Vue 공부하기", chk: false },
-      { id: 2, name: "TV 시청'", chk: false },
+      { id: 2, name: "TV 시청", chk: false },
       { id: 3, name: "저녁운동", chk: false },
     ],
     title: "Hello, world",
@@ -17,6 +17,18 @@ export default createStore({
     },
     todoList(state) {
       return state.todos;
+    },
+  },
+  mutations: {
+    // 기능구현
+    addTodo(state, name) {
+      const id = state.todos[state.todos.length - 1].id;
+      const todo = {
+        id: id + 1,
+        name: name,
+        chk: false,
+      };
+      state.todos.push(todo);
     },
   },
 });
